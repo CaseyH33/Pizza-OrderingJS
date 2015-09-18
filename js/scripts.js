6f66b53a93ea5291a597af01be15d7cf584c5e57
Casey Heitz <Heitz.Casey@gmail.com>
2015-09-18 10:02:38 -0700

    Add function to list all toppings using checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,12 @@ function listPizzaSizes(pizzaSizes) {
   });
 }
 
+function listToppings(toppingsList) {
+  toppingsList.forEach(function(topping) {
+    $("#topping-list").append('<li><input type="checkbox" value=' + topping + '>' + topping.toppingItem + ' +$' + topping.toppingCost + '</li>');
+  });
+}
+
 var pizzaSizes = [];
 var small = new PizzaSize("Small", 7);
 var medium = new PizzaSize("Medium", 10);
@@ -35,8 +41,24 @@ var large = new PizzaSize("Large", 14);
 var extraLarge = new PizzaSize("Extra Large", 20);
 pizzaSizes.push(small, medium, large, extraLarge);
 
+var toppingsList = [];
+var pepperoni = new Topping("Pepperoni", 2);
+var sausage = new Topping("Sausage", 2);
+var canadianBacon = new Topping("Canadian Bacon", 2);
+var bacon = new Topping("Bacon", 3);
+var salami = new Topping("Salami", 3);
+var olives = new Topping("Olives", 1);
+var mushrooms = new Topping("Mushrooms", 1);
+var greenPeppers = new Topping("Green Peppers", 1);
+var pineapple = new Topping("Pineapple", 3);
+var jalapenos = new Topping("Jalapeno", 1);
+var extraCheese = new Topping("Extra Cheese", 3);
+toppingsList.push(pepperoni, sausage, canadianBacon, bacon, salami, olives, mushrooms, greenPeppers, pineapple, jalapenos, extraCheese);
+
+
 $(document).ready(function() {
   listPizzaSizes(pizzaSizes);
+  listToppings(toppingsList);
   $("form#new-order").submit(function(event) {
     event.preventDefault();
 
