92645b139a68d00e6d9f57878200fcd23ff2ac42
Casey Heitz <Heitz.Casey@gmail.com>
2015-09-18 09:00:00 -0700

    Add passing test for pizza cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ function Pizza(quantity, pizzaSize, toppings) {
     this.toppings = toppings;
 }
 
+Pizza.prototype.price = function() {
+  var cost = this.pizzaSize.sizeCost;
+  this.toppings.forEach(function(topping) {
+    cost += topping.toppingCost;
+  });
+  return cost*this.quantity;
+}
+
 function Topping(toppingItem, toppingCost) {
     this.toppingItem = toppingItem;
     this.toppingCost = toppingCost;
--- spec/specs.js
+++ spec/specs.js
@@ -1,26 +1,22 @@
 describe('Pizza', function() {
   it("creates a new pizza with the given specifications", function() {
     var quantity = 1;
-    var pizzaSize = "medium";
+    var pizzaSize = ["medium", 10];
     var toppings = ["cheese", "pepperoni"];
     var testPizza = new Pizza(quantity, pizzaSize, toppings);
     expect(testPizza.quantity).to.equal(1);
-    expect(testPizza.pizzaSize).to.equal("medium");
+    expect(testPizza.pizzaSize).to.eql(["medium", 10]);
     expect(testPizza.toppings).to.eql(["cheese", "pepperoni"]);
   });
 
-  // it("calculates the cost of a pizza using the given specifications", function() {
-  //   var toppingItem1 = "cheese";
-  //   var toppingCost1 = 1;
-  //   var toppingItem2 = "pepperoni";
-  //   var toppingCost2 = 3;
-  //   var pizzaSize = "medium";
-  //   var toppings = ["cheese", "pepperoni"];
-  //   var testPizza = new Pizza(quantity, pizzaSize, toppings);
-  //   expect(testPizza.quantity).to.equal(1);
-  //   expect(testPizza.pizzaSize).to.equal("medium");
-  //   expect(testPizza.toppings).to.eql(["cheese", "pepperoni"]);
-  //   });
+  it("calculates the cost of a pizza using the given specifications", function() {
+    var testQuantity = 2;
+    var testPizzaSize = new PizzaSize("medium", 10);
+    var testTopping1 = new Topping("cheese", 1);
+    var testTopping2 = new Topping("pepperoni", 3);
+    var testPizza = new Pizza(testQuantity, testPizzaSize, [testTopping1, testTopping2]);
+    expect(testPizza.price()).to.equal(28);
+    });
 });
 
 describe('Topping', function() {
