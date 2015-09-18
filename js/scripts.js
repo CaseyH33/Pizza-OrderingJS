4881f009b87313557d89e14bb249adc8c5e43091
Casey Heitz <Heitz.Casey@gmail.com>
2015-09-18 09:39:15 -0700

    Add function to list all pizza sizes in a drop down menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function Pizza(quantity, pizzaSize, toppings) {
-    this.quantity = quantity;
-    this.pizzaSize = pizzaSize;
-    this.toppings = toppings;
+  this.quantity = quantity;
+  this.pizzaSize = pizzaSize;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.price = function() {
@@ -13,11 +13,33 @@ Pizza.prototype.price = function() {
 }
 
 function Topping(toppingItem, toppingCost) {
-    this.toppingItem = toppingItem;
-    this.toppingCost = toppingCost;
+  this.toppingItem = toppingItem;
+  this.toppingCost = toppingCost;
 }
 
 function PizzaSize(sizeName, sizeCost) {
-    this.sizeName = sizeName;
-    this.sizeCost = sizeCost;
+  this.sizeName = sizeName;
+  this.sizeCost = sizeCost;
 }
+
+function listPizzaSizes(pizzaSizes) {
+  pizzaSizes.forEach(function(size) {
+    $("#pizza-size").append('<option value=' + size + '>' + size.sizeName + ' ($' + size.sizeCost + ')' + '</option>');
+  });
+}
+
+var pizzaSizes = [];
+var small = new PizzaSize("Small", 7);
+var medium = new PizzaSize("Medium", 10);
+var large = new PizzaSize("Large", 14);
+var extraLarge = new PizzaSize("Extra Large", 20);
+pizzaSizes.push(small, medium, large, extraLarge);
+
+$(document).ready(function() {
+  listPizzaSizes(pizzaSizes);
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedQuantity = parseInt($("input#quantity").val());
+  })
+});
